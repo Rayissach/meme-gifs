54266c9893686b07928df40765ec8e47bbdd4a75
rayissach <rayissach@gmail.com>
2018-02-10 13:00:49 -0800

    Adding personal unique api key

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,46 +1,72 @@
-  $("button").on("click", function() {
-      // Grabbing and storing the data-animal property value from the button
-      var wolrdStar = $(this).attr("button-container");
+//When button clicked start this function
+// $("#button-container").on("click", function() {
+  //Query url with api key
+  var buttonArr = []
+ 
+  var queryURL = "https://api.giphy.com/v1/gifs?api_key=ICK0911MJ3OSyCVmYJsRtqgKZ0CzLbhW"
 
-      // Constructing a queryURL using the animal name
-      var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-        wolrdStar + "&api_key=dc6zaTOxFJmzC";
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function(response) {
+    console.log(response.data);
+    console.log(response.data.image_original_url);
 
-      $.ajax({
-          url: queryURL,
-          method: "GET"
-        })
-        .then(function(response) {
-          console.log(queryURL);
+    var gif = (response.data.image_original_url);
 
-          console.log(response);
-          var results = response.data;
+    var imageUrl = $("<img>");
 
-          // Looping through each result item
+    imageUrl.attr("src", gif);
 
-          for (var i = 0; i < results.length; i++) {
+    $("#button-container").append(imageUrl)
+  });
 
-            // Creating and storing a div tag
-            var gifDiv = $("<div>");
 
-            // Creating a paragraph tag with the result item's rating
-            console.log(results[i])
-            var restrict = $("<p>").text("Rating: " + results[i].rating);
+// });
 
-            // Creating and storing an image tag
-            var WorldStarImage = $("<img>");
-            // Setting the src attribute of the image to a property pulled off the result item
-            WorldStarImage.attr("src", results[i].images.fixed_height.url);
+  // $("button").on("click", function() {
+  //     // Grabbing and storing the data-animal property value from the button
+  //     var wolrdStar = $(this).attr("button-container");
 
-            // Appending the paragraph and image tag to the animalDiv
-            animalDiv.append(p);
-            animalDiv.append(animalImage);
+  //     // Constructing a queryURL using the animal name
+  //     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+  //       wolrdStar + "&api_key=dc6zaTOxFJmzC";
 
-            // Prependng the animalDiv to the HTML page in the "#gifs-appear-here" div
-            $("#gif-content").prepend(animalDiv);
-          }
-        });
-    });
+  //     $.ajax({
+  //         url: queryURL,
+  //         method: "GET"
+  //       })
+  //       .then(function(response) {
+  //         console.log(queryURL);
+
+  //         console.log(response);
+  //         var results = response.data;
+
+  //         // Looping through each result item
+
+  //         for (var i = 0; i < results.length; i++) {
+
+  //           // Creating and storing a div tag
+  //           var gifDiv = $("<div>");
+
+  //           // Creating a paragraph tag with the result item's rating
+  //           console.log(results[i])
+  //           var restrict = $("<p>").text("Rating: " + results[i].rating);
+
+  //           // Creating and storing an image tag
+  //           var WorldStarImage = $("<img>");
+  //           // Setting the src attribute of the image to a property pulled off the result item
+  //           WorldStarImage.attr("src", results[i].images.fixed_height.url);
+
+  //           // Appending the paragraph and image tag to the animalDiv
+  //           animalDiv.append(p);
+  //           animalDiv.append(animalImage);
+
+  //           // Prependng the animalDiv to the HTML page in the "#gifs-appear-here" div
+  //           $("#gif-content").prepend(animalDiv);
+  //         }
+  //       });
+  //   });
   
 
 
@@ -63,18 +89,18 @@
 //         })
 //         .then(function(response) {
 //           var results = response.data;
- function btnAdd () {
- 	for (var i = 0; i < wolrdStar.length; i++) {
- 		console.log(wolrdStar[i]);
- 		var buttons = $("<button>");
- 		buttons.addClass("btn gifs")
- 		buttons.text(wolrdStar[i]);
+ // function btnAdd () {
+ // 	for (var i = 0; i < wolrdStar.length; i++) {
+ // 		console.log(wolrdStar[i]);
+ // 		var buttons = $("<button>");
+ // 		buttons.addClass("btn gifs")
+ // 		buttons.text(wolrdStar[i]);
  		
- 		$("#button-container").append(buttons);
- 	}
- }
+ // 		$("#button-container").append(buttons);
+ // 	}
+ // }
 
- btnAdd();
+ // btnAdd();
 
 
 // //grabs the text from the button clicked with a class animal.
